bf86f8c8393904029756a8c8a358839986034494
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-02-12 17:25:30 +0300

    feat: added home page

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div className="bg-black text-white h-full justify-center items-center flex">
-      HomePage
+    <div className="bg-black text-white h-full justify-center items-center flex flex-col gap-4">
+      <p className="text-[24px] text-center">Jr. Front End Developer</p>
+      <h1 className="font-stretch-pro! text-center text-[72px] leading-[73px]">
+        MUZAFFER<br></br>YILDIRIM
+      </h1>
     </div>
   );
 }
