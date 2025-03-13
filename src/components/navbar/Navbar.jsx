8014ce4a09fb5e3c6d73610035e67a52ebffcc26
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-03-13 15:12:14 +0300

    feat: added responsive layout for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.svg";
 import { Link, useLocation } from "react-router-dom";
+import style from "./style.module.scss";
 
 function Navbar() {
   const { pathname } = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const linkStyle = (path) =>
     pathname === path
@@ -35,18 +37,37 @@ function Navbar() {
   };
 
   return (
-    <div className="flex justify-between p-12 bg-black border-b-1 border-white">
-      <div className="logo-container w-auto h-auto">
-        <img src={logo} alt="MY" width={78} height={34} />
-      </div>
+    <div>
+      <div
+        className={`${
+          isOpen ? "h-screen absolute w-full flex-col z-10" : ""
+        } flex justify-between p-12 bg-black border-b-1 border-white`}
+      >
+        <div className="md:w-auto h-auto flex justify-between w-full">
+          <img src={logo} alt="MY" width={78} height={34} />
 
-      <div className="links flex gap-8 items-center">
-        {links.map((link, index) => (
-          <Link key={index} to={link.path} className={linkStyle(link.path)}>
-            {link.label}
-            {getIcon(link.path) && <i className={getIcon(link.path)}></i>}
-          </Link>
-        ))}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-[24px] text-white md:hidden"
+          >
+            <i className="bi bi-list"></i>
+          </button>
+        </div>
+
+        <div
+          className={`${
+            isOpen
+              ? "flex flex-col relative w-full h-full justify-center"
+              : "hidden"
+          } links gap-8 items-center md:flex`}
+        >
+          {links.map((link, index) => (
+            <Link key={index} to={link.path} className={linkStyle(link.path)}>
+              {link.label}
+              {getIcon(link.path) && <i className={getIcon(link.path)}></i>}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div className="bg-black text-white h-full justify-center items-center flex flex-col gap-4">
       <p className="text-[24px] text-center">Jr. Front End Developer</p>
-      <h1 className="font-stretch-pro! text-center text-[72px] leading-[73px]">
+      <h1 className="font-stretch-pro! text-center text-[64px] leading-[64px]">
         MUZAFFER<br></br>YILDIRIM
       </h1>
     </div>
