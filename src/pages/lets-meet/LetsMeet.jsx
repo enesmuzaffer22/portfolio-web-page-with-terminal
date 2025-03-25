2e5d61577fe33b6e53d1e3449a93aff81cbe2c96
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-03-26 01:56:20 +0300

    feat: added lets meet page

--- src/pages/lets-meet/LetsMeet.jsx
+++ src/pages/lets-meet/LetsMeet.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import LetsMeetImg from "../../assets/lets-meet-img.jpg";
 
 function LetsMeet() {
-  return <div>LetsMeet</div>;
+  return (
+    <div className="bg-black flex h-full w-full justify-center items-center">
+      <div className="flex items-center gap-16 w-[80%] xl:w-[1000px]">
+        <img src={LetsMeetImg} alt="" className="rounded-lg hidden lg:flex" />
+        <p className="text-white text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px]">
+          Hello, I’m Muzaffer. I’m a final-year Computer Engineering student at
+          Istanbul University of Health and Technology. I am pursuing my career
+          as a <b>Front-End Developer</b> and I am continuously working on
+          improving myself in this field. <br></br>
+          <br></br>The programming languages and frameworks I actively use:{" "}
+          <br></br>
+          <br></br>
+          <b>
+            <i>JavaScript, React, Angular, Tailwind and SASS.</i>
+          </b>
+          <br></br>
+          <br></br>Additionally, I am also interested in graphic design as a
+          hobby. I try to stay active in this area as well. <br></br>
+          <br></br>The design tools I actively use:
+          <br></br>
+          <br></br>
+          <b>
+            <i>Figma, Adobe Photoshop and Adobe Illustrator.</i>
+          </b>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LetsMeet;
