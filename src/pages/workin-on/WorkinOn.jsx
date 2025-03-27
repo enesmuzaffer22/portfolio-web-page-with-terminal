6243ab0c8b11b89fda413c514f8e73a590fcf1d3
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-03-27 15:10:08 +0300

    feat: added layout for workin on page

--- src/pages/workin-on/WorkinOn.jsx
+++ src/pages/workin-on/WorkinOn.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import WorkCard from "../../components/work-card/WorkCard";
 
 function WorkinOn() {
-  return <div>WorkinOn</div>;
+  return (
+    <div className="bg-black h-full w-full flex items-center justify-center flex-col gap-6">
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+    </div>
+  );
 }
 
 export default WorkinOn;
