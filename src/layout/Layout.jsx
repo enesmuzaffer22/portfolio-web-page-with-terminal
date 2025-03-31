70218d536741915e077e4c4ef2efd53fc2160f8c
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-04-01 00:01:03 +0300

    feat: added new features for shell

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import TerminalScreen from "../components/terminal-screen/TerminalScreen";
 
 function Layout() {
+  const [isShellOpen, setIsShellOpen] = useState(false);
+
+  const handleShellOpen = (data) => {
+    setIsShellOpen(data);
+  };
+
   return (
     <>
-      <TerminalScreen />
+      <TerminalScreen
+        isShellOpenProp={isShellOpen}
+        setIsShellOpen={setIsShellOpen}
+      />
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <Outlet />
-        <Footer />
+        <Footer toggleShell={handleShellOpen} />
       </div>
     </>
   );
